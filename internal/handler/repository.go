719084fac004b83b3231d724164ee29925c74b32
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-05-20 18:48:49 +0200

    Run Teamkatalogen calls in parallel

--- internal/handler/repository.go
+++ internal/handler/repository.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"sync"
 
 	"github.com/navikt/whodis/internal/github"
 	"github.com/navikt/whodis/internal/teamkatalogen"
@@ -15,31 +16,38 @@ type Repository struct {
 
 func (repo *Repository) Owners(w http.ResponseWriter, r *http.Request) {
 	repoName := r.PathValue("repoName")
-	owners, err := repo.GitHubClient.AdminsFor(repoName)
+	repoAdmins, err := repo.GitHubClient.AdminsFor(repoName)
 	if err != nil {
-		slog.Error("error getting repo owners", slog.Any("error", err))
+		slog.Error("error getting repo admins", slog.Any("error", err))
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-	var reply []ownerInfo
-	for _, owner := range owners {
-		ownerDetails, err := teamkatalogen.DetailsForUser(repo.GitHubClient.EmailFor(owner))
-		if err != nil {
-			slog.Error("error getting repo owner details", slog.Any("error", err))
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		reply = append(reply, ownerInfo{
-			GitHubUsername: owner,
-			WorkEmail:      repo.GitHubClient.EmailFor(owner),
-			Teams:          ownerDetails.Teams,
-		})
+	var wg sync.WaitGroup
+	wg.Add(len(repoAdmins))
+	var reply []adminDetails
+	for _, admin := range repoAdmins {
+		go func() {
+			teamkatalogenReponse, err := teamkatalogen.DetailsForUser(repo.GitHubClient.EmailFor(admin))
+			defer wg.Done()
+			if err != nil {
+				slog.Error("error getting repo admin details", slog.Any("error", err))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			reply = append(reply, adminDetails{
+				GitHubUsername: admin,
+				WorkEmail:      repo.GitHubClient.EmailFor(admin),
+				Teams:          teamkatalogenReponse.Teams,
+			})
+		}()
 	}
+	wg.Wait()
 	if err := json.NewEncoder(w).Encode(reply); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
-type ownerInfo struct {
+type adminDetails struct {
 	GitHubUsername string               `json:"github_username"`
 	WorkEmail      string               `json:"work_email"`
 	Teams          []teamkatalogen.Team `json:"teams"`
