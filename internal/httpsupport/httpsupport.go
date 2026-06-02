da846b677df2a362f5dceffa771e7f9a31f1d48a
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-06-02 09:44:03 +0200

    Will quotes be auto-escaped?

--- internal/httpsupport/httpsupport.go
+++ internal/httpsupport/httpsupport.go
@@ -74,9 +74,8 @@ func MakePostRequest(uri string, authToken string, reqBody []byte) ([]byte, erro
 
 func MakeGqlQuery[T any](uri string, authToken string, query string) (*T, error) {
 	queryAsSingleLine := strings.Replace(query, "\n", " ", -1)
-	queryAsSingleLine = strings.Replace(query, `"`, `\"`, -1)
 	reqBody := map[string]string{
-		"query": queryAsSingleLine,
+		"query": fmt.Sprintf(`%s`, queryAsSingleLine),
 	}
 	var buf bytes.Buffer
 	if err := json.NewEncoder(&buf).Encode(reqBody); err != nil {
