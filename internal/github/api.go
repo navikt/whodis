b4aba0c47b18710967e2179947f540dd439d512f
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-06-08 08:40:21 +0200

    Removed duplicate logging

--- internal/github/api.go
+++ internal/github/api.go
@@ -1,7 +1,5 @@
 package github
 
-import "log/slog"
-
 var samlUsersQuery = `query {
   organization(login: \"navikt\") {
     samlIdentityProvider {
@@ -70,7 +68,6 @@ func (resp *samlUsersResponse) AsMap() map[string]string {
 		key := edge.Node.User.Login
 		m[key] = edge.Node.SamlIdentity.Emails[0].Value
 	}
-	slog.Info("Loaded users from GitHub", slog.Int("count", len(m)), slog.Int("errors", errorCont))
 	return m
 }
 
